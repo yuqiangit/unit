212f82787551cbb13d0ee34162b23891e8712285
walter <langlive@lang.local>
2017-01-18 12:04:54 +0800

    本地php练习1

---
+++ array/array_info.php
@@ -0,0 +1,406 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: langlive
+ * Date: 16/12/28
+ * Time: 下午6:01
+ */
+$arr =  array (
+    'welcome_ad' =>
+        array (
+            0 =>
+                array (
+                    'id' => '217',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/059b10e2181ed509975f069202b4d4c0',
+                    'jump_url' => 'http://www.langlive.com/',
+                    'live_id' => '',
+                    'start_time' => '1480521600',
+                    'end_time' => '1514649600',
+                    'sec' => '3',
+                ),
+            1 =>
+                array (
+                    'id' => '205',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '开台庆活动',
+                    'live_img' => '',
+                    'jump_url' => 'http://s.api.langlive.com/html/activity/activity.html?key=jnTXHP&access_token=uxosBV',
+                    'live_id' => '',
+                    'start_time' => '1482406800',
+                    'end_time' => '1483005631',
+                    'sec' => '5',
+                ),
+        ),
+    'hot_across_ad' =>
+        array (
+            0 =>
+                array (
+                    'id' => '86',
+                    'type' => '1',
+                    'pfid' => '1000058',
+                    'title' => '填問卷抽iPad mini 4 ',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/4e90dde326a7c00c5b16802177c038e5',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            1 =>
+                array (
+                    'id' => '124',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/75e660850feb80c1ba1048282d1a0a94',
+                    'jump_url' => 'http://s.api.langlive.com/html/pk',
+                    'live_id' => 'NULL',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            2 =>
+                array (
+                    'id' => '159',
+                    'type' => '5',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215102935_20161215142651.m3u8',
+                    'jump_url' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215114123_20161215114945.m3u8',
+                    'live_id' => '10008401481768975',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'stream_direction' => '1',
+                    'sec' => '3',
+                ),
+            3 =>
+                array (
+                    'id' => '163',
+                    'type' => '6',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/49914f7f478d18c5001f051b2c074952',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            4 =>
+                array (
+                    'id' => '173',
+                    'type' => '1',
+                    'pfid' => '1000840',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/1d12281c5d63bd7a6a31922b31c1b7d7',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            5 =>
+                array (
+                    'id' => '220',
+                    'type' => '1',
+                    'pfid' => '1000660',
+                    'title' => '',
+                    'live_img' => '',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+        ),
+    'topic_top_ad' =>
+        array (
+            0 =>
+                array (
+                    'id' => '106',
+                    'type' => '1',
+                    'pfid' => '1000058',
+                    'title' => '開台慶活動',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/850bab1530c62027ddcf5de0d33c8b2c',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            1 =>
+                array (
+                    'id' => '160',
+                    'type' => '5',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://video-hls.langlive.com.tw/live/10001491479095517/10001491479095517_20161114115203_20161114122723.m3u8',
+                    'jump_url' => 'http://video-hls.langlive.com/live/10001491479095517/reviewlist.m3u8?starttime=1479095523&endtime=1479097643',
+                    'live_id' => '10001491479095517',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'stream_direction' => '0',
+                    'sec' => '3',
+                ),
+            2 =>
+                array (
+                    'id' => '164',
+                    'type' => '6',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/5c3f3293a8efbdcfa819979f3be860d0',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            3 =>
+                array (
+                    'id' => '171',
+                    'type' => '1',
+                    'pfid' => '1000840',
+                    'title' => '横屏',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/10eb825edb0cc1c09f7355ecf64c76ff',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            4 =>
+                array (
+                    'id' => '188',
+                    'type' => '5',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215102935_20161215142651.m3u8',
+                    'jump_url' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215114123_20161215114945.m3u8',
+                    'live_id' => '10008401481768975',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'stream_direction' => '1',
+                    'sec' => '3',
+                ),
+            5 =>
+                array (
+                    'id' => '93',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '222',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/69da4c2ddfef9661d71893e04daf941a',
+                    'jump_url' => 'https://www.baidu.com',
+                    'live_id' => 'NULL',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            6 =>
+                array (
+                    'id' => '94',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '111',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/adc3f8f3c40e6539e35c5646cccee194',
+                    'jump_url' => 'www.baidu.com',
+                    'live_id' => 'NULL',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+        ),
+    'hot_top_ad' =>
+        array (
+            0 =>
+                array (
+                    'id' => '123',
+                    'type' => '5',
+                    'pfid' => '0',
+                    'title' => '回放',
+                    'live_img' => 'http://video-hls.langlive.com.tw/live/10001491479095517/10001491479095517_20161114115203_20161114122723.m3u8',
+                    'jump_url' => 'http://video-hls.langlive.com/live/10001491479095517/reviewlist.m3u8?starttime=1479095523&endtime=1479097643',
+                    'live_id' => '10001491479095517',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'stream_direction' => '0',
+                    'sec' => '3',
+                ),
+            1 =>
+                array (
+                    'id' => '126',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/9597eebd956dc825d943ee8e08f02a65',
+                    'jump_url' => 'http://s.api.langlive.com/html/my/img_upload.html',
+                    'live_id' => '',
+                    'start_time' => '1480300000',
+                    'end_time' => '1483113600',
+                    'sec' => '3',
+                ),
+            2 =>
+                array (
+                    'id' => '184',
+                    'type' => '5',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215102935_20161215142651.m3u8',
+                    'jump_url' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215114123_20161215114945.m3u8',
+                    'live_id' => '10008401481768975',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'stream_direction' => '1',
+                    'sec' => '3',
+                ),
+            3 =>
+                array (
+                    'id' => '195',
+                    'type' => '6',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/10c51b8eb8fea2662fcf69ffbb59e6c0',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            4 =>
+                array (
+                    'id' => '119',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '前端测试',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/16877dc7b336dc4eedbc13b5aa67755e',
+                    'jump_url' => 'http://s.api.langlive.com/html/apptest.html',
+                    'live_id' => '',
+                    'start_time' => '1479873060',
+                    'end_time' => '1483027200',
+                    'sec' => '3',
+                ),
+            5 =>
+                array (
+                    'id' => '113',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '音浪杯',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.cn/ef5f85cb0500e6fe67e356967ca58fa8',
+                    'jump_url' => 'http://s.api.langlive.com/html/activity/pk_activity_h5',
+                    'live_id' => 'NULL',
+                    'start_time' => '1481005620',
+                    'end_time' => '1483718400',
+                    'sec' => '3',
+                ),
+            6 =>
+                array (
+                    'id' => '196',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '前端人气火浪浪测试',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/e7fbd7b7b2458f3f7b498b6e338104b0',
+                    'jump_url' => 'http://s.api.langlive.com/html/activity/hot.html',
+                    'live_id' => '',
+                    'start_time' => '1482076800',
+                    'end_time' => '1483027200',
+                    'sec' => '3',
+                ),
+        ),
+    'topic_across_ad' =>
+        array (
+            0 =>
+                array (
+                    'id' => '161',
+                    'type' => '6',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/01f979c18e434bd2151c67d2c278f063',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            1 =>
+                array (
+                    'id' => '165',
+                    'type' => '5',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215102935_20161215142651.m3u8',
+                    'jump_url' => 'http://video-hls.langlive.com.tw/live/10008401481768975/10008401481768975_20161215114123_20161215114945.m3u8',
+                    'live_id' => '10008401481768975',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'stream_direction' => '1',
+                    'sec' => '3',
+                ),
+            2 =>
+                array (
+                    'id' => '172',
+                    'type' => '1',
+                    'pfid' => '1000840',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/24a242ab5efeaf93ff04674ce747dcc5',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            3 =>
+                array (
+                    'id' => '83',
+                    'type' => '1',
+                    'pfid' => '1000058',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/364fa7f888a1f6ae7ca4a7aa3e0f028c',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+        ),
+    'pop_up_ad' =>
+        array (
+            0 =>
+                array (
+                    'id' => '218',
+                    'type' => '2',
+                    'pfid' => '0',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/bfaedf39a8647b8d46010b9113743ae9',
+                    'jump_url' => 'http://www.baidu.com/',
+                    'live_id' => '',
+                    'start_time' => '1482832831',
+                    'end_time' => '1483005631',
+                    'sec' => '3',
+                ),
+            1 =>
+                array (
+                    'id' => '219',
+                    'type' => '1',
+                    'pfid' => '1000058',
+                    'title' => '',
+                    'live_img' => 'http://playback-langlive.ufile.ucloud.com.cn/7241e49bafa7145fb61529fcd8808ad1',
+                    'jump_url' => '',
+                    'live_id' => '',
+                    'start_time' => '1480521600',
+                    'end_time' => '1483632000',
+                    'sec' => '3',
+                ),
+        ),
+);
+
+$sort_by = ['hot_across_ad','hot_top_ad','topic_top_ad','topic_across_ad','welcome_ad','pop_up_ad'];
+ksort($arr);
+//array_multisort($sort_by,SORT_DESC,$arr);
+
+
+foreach ($arr as $k => $v){
+    var_dump($k);
+}
