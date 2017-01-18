2f5d6b6623367b5eda79b347c6c2be59bf1834f9
walter <langlive@lang.local>
2017-01-18 11:48:54 +0800

    本地php练习

---
+++ array/array_value.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: langlive
+ * Date: 16/12/5
+ * Time: 下午12:18
+ */
+$array = ['100','100','101','120','100','100','120','101'];
+var_dump(array_values($array));exit;
