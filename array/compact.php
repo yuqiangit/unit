a6f8f4f905ec101d7854df2bbdf8dab109f23558
walter <langlive@lang.local>
2017-01-18 12:01:50 +0800

    本地php练习

---
+++ array/compact.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: langlive
+ * Date: 16/12/28
+ * Time: 下午3:46
+ */
+$id = 1;
+$info = compact('id', 'type', 'pfid', 'title', 'live_img', 'jump_url', 'live_id', 'start_time', 'end_time','playback_url','stream_direction');
+var_dump($info);exit;
