c9256fdda2f26d29bd1fe4ac1ea3fea41d5c6113
walter <langlive@lang.local>
2017-01-23 11:34:31 +0800

    oop php 对象

---
+++ array/array_pop.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: langlive
+ * Date: 17/1/22
+ * Time: 下午6:16
+ */
+$arr = ['tail'=>'a','b','c'];
+var_dump(array_slice($arr,0,1));
---
+++ array/unique.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: langlive
+ * Date: 17/1/18
+ * Time: 下午6:49
+ */
+$arr = ['1'=>'a','2'=>'b','3' =>'b'];
+$arr = array_unique($arr);
+var_dump($arr);
